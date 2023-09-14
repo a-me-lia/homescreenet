c9eb3807c09f1926258c12e49a9e4442b669e431
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-09-14 17:03:17 -0400

    added sitemap
    Signed-off-by: matthewguo86 <110259342+a-me-lia@users.noreply.github.com>

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://homescree.net/sitemap.xml',
+    host: 'https://homescree.net.io',
+  };
+}
