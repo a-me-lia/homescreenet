4ef174d4aa052f193c6ed3976a43a51266303e84
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-09-15 12:05:57 -0400

    no more google
    Signed-off-by: matthewguo86 <110259342+a-me-lia@users.noreply.github.com>

--- src/app/guestbook/client.tsx
+++ src/app/guestbook/client.tsx
@@ -3,38 +3,23 @@ import { SignIn, SignOut } from "./buttons";
 import Form from "./form";
 import { useSession } from "next-auth/react";
 
-import { app } from "@/lib/firebase";
-import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, User } from 'firebase/auth'
-import { sign } from "crypto";
+import { app } from "@/lib/firebase"
 
 
-
-let user:User | undefined, credential, token;
-const gAuth = getAuth(app);
-
-const provider = new GoogleAuthProvider()
-provider.setCustomParameters({ prompt: "select_account" });
-provider.addScope('profile');
-provider.addScope('email');
-
-async function signInGoogle() {
-
-
-  await signInWithRedirect(gAuth, provider);
-
-  const result = await getRedirectResult(gAuth);
-
-
-  if (result) {
-     user = result.user;
-     credential = GoogleAuthProvider.credentialFromResult(result);
-     token = credential?.accessToken;
-  }
-}
 
 export default function Client() {
-  const { data: session } = useSession();
-  let user = session?.user;
+  let user: undefined | {
+    name?: string | null | undefined;
+    email?: string | null | undefined;
+    image?: string | null | undefined;
+} = undefined;
+
+
+
+  const { data: session } = useSession(); 
+  user = session?.user
+
+
 
   return (
     <>
@@ -45,7 +30,7 @@ export default function Client() {
           <SignOut />
         </>
       ) : (
-        <SignIn gFunc={async() => await signInGoogle()} />
+        <SignIn />
       )}
     </>
   );
