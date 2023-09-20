89da26b3aa74403d345cf175652ccc063881b694
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-09-20 07:37:41 -0400

    bigger navbar
    Signed-off-by: matthewguo86 <110259342+a-me-lia@users.noreply.github.com>

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -54,8 +54,8 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav className="flex flex-col ">
-      <div className="md:mx-auto md:w-[742px]  mx-4 h-max  flex flex-col fixed right-0 top-0 left-0 z-50 bg-gradient-to-b pb-4 from-white via-white to-transparent via-90% font-mono ">
+    <nav className="flex flex-col text-[18px] tracking-tighterfont-mono ">
+      <div className="md:mx-auto md:w-[742px]  mx-4 h-max  flex flex-col fixed right-0 top-0 left-0 z-50 bg-gradient-to-b pb-4 from-white via-white to-transparent via-90% ">
         <div className="flex flex-row md:mt-8 mt-10 ">
           <div className="flex flex-row items-baseline -mr-6 ">
             <Link href={"/"} className="md:block hidden" id="homescree.net">
