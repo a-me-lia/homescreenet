334959c0a0f13bd9b8010f2e450aa0830697752a
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-09-20 13:31:53 -0400

    styled ext link
    Signed-off-by: matthewguo86 <110259342+a-me-lia@users.noreply.github.com>

--- src/app/components/mdx.tsx
+++ src/app/components/mdx.tsx
@@ -22,16 +22,17 @@ const CustomLink = (props: any) => {
   }
 
   return (
-    <div className="flex flex-row items-center">
+    <div className="flex flex-row ">
       <a
-        className="font-semibold"
+        className="font-semibold text-blue-400 hover:underline"
         target="_blank"
         rel="noopener noreferrer"
         {...props}
       />
       <svg
-        width="12px"
-        height="12px"
+        className="mt-1 ml-2"
+        width="16px"
+        height="16px"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
