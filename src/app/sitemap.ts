5a759afc64ab9419cda13aae4f7724f02b0b08aa
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-09-20 13:34:28 -0400

    updated sitemap
    Signed-off-by: matthewguo86 <110259342+a-me-lia@users.noreply.github.com>

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,20 @@
-import { allBlogs } from "@/../.contentlayer/generated";
+import { allBlogs, allAboutPages } from "@/../.contentlayer/generated";
 
 export default async function sitemap() {
   const blogs = allBlogs.map((post) => ({
     url: `https://homescree.net/blog/${post.slug}`,
     lastModified: post.publishedAt,
+  }));  
+
+  const about = allAboutPages.map((post) => ({
+    url: `https://homescree.net/home/about/${post.slug}`,
+    lastModified: post.publishedAt,
   }));
 
-  const routes = ["", "/blog", "/guestbook", "/contact"].map((route) => ({
+  const routes = ["", "/home", "/home/about", "/home/about/resume", "/blog", "/guestbook", "/contact"].map((route) => ({
     url: `https://homescree.net${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes, ...blogs, ...about];
 }
