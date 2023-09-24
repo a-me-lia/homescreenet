e271976dba1f3ab1978103be7b1f56529007a918
marin <marinkitagawa.x86@gmail.com>
2023-09-24 14:41:59 -0400

    clears success msg
    Signed-off-by: marin <marinkitagawa.x86@gmail.com>

--- src/app/contact/form.tsx
+++ src/app/contact/form.tsx
@@ -27,6 +27,7 @@ useEffect(()=>{
 }, [name, text, email])
 
   function displayWhyCantSubmit(){
+    setState('')
     const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
     function validateEmail(email:string){
       return emailRegex.test(email);
